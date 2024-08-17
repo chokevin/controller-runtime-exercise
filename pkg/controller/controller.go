0e33c9915ece7b8be4f7263bdace7a856191d8c7
chokevin <chokevin@hawaii.edu>
2024-08-16 17:36:41 -0700

    fixing it to pass in args instead

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -152,9 +152,9 @@ func createDeployment(myApp *api.MyApp) *appv1.Deployment {
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
-							Name:    myApp.Name,
-							Image:   myApp.Spec.Image,
-							Command: myApp.Spec.Args,
+							Name:  myApp.Name,
+							Image: myApp.Spec.Image,
+							Args:  myApp.Spec.Args,
 							Resources: corev1.ResourceRequirements{
 								Requests: corev1.ResourceList{
 									corev1.ResourceCPU:    resource.MustParse("100m"),
